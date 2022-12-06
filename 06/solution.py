d91b534926d14622e4bfc5566f64ecfab3d28480
Synth <piplup654@protonmail.com>
2022-12-06 09:33:55 +0100

    Day6 code golf solution

--- 06/solution.py
+++ 06/solution.py
@@ -9,9 +9,11 @@ def part1(data):
                 return i+1
 def part2(data):
     for i in range(len(data)):
-        if i -13 > 0:
+        if i - 13 > 0:
             marker = len(set([data[i-j] for j in range(14)]))
             if marker == 14:
                 return i+1
-print(part1(parse_input("input.txt")))
-print(part2(parse_input("input.txt")))
+def codeGolfSolution(data, dist_len):
+    return [i+1 for i in range(len(data)) if i - dist_len-1 > 0 and len(set([data[i-j] for j in range(dist_len)])) == dist_len][0]
+print(codeGolfSolution(parse_input("input.txt"), 4))
+print(codeGolfSolution(parse_input("input.txt"), 14))
